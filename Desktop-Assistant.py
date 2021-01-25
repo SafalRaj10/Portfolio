c0fed97c2ae270090e7cd36b5122cb58a4db72e4
SafalRaj10 <73661904+SafalRaj10@users.noreply.github.com>
2021-01-25 19:47:08 +0530

    Update Desktop-Assistant.py

--- Desktop-Assistant.py
+++ Desktop-Assistant.py
@@ -7,7 +7,6 @@ import webbrowser
 import pyautogui
 from tkinter import filedialog
 import random
-os.chdir("C:/Users/91785/PycharmProjects/pythonProject")
 engine = pyttsx3.init('sapi5')
 engine.setProperty('rate', 165)
 engine.setProperty('volume', 1)
@@ -17,8 +16,7 @@ def speak(speak):
     engine.say(speak)
     engine.runAndWait()
 # speak("Engine Started")
-# speak("I can now listen")
-i=1
+# speak("Good Morning")
 toast=ToastNotifier()
 toast.show_toast("Voice Assistant","Voice Assist has started", duration=3)    #For notification in windows
 def takescreenshot1():
@@ -54,18 +52,15 @@ while True:
         elif ("open microsoft edge" in data or "open edge" in data or "open age" in data or "edge open" in data or "age open" in data or "h open" in data):
             print("opening Edge")
             os.startfile("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe")
-            continue
         elif ("open chrome" in data or "open google chrome" in data or "chrome open" in data or "google chrome" in data or "chrome kholo" in data or "google chrome kholo" in data):
             print("opening Chrome")
             os.startfile('C:\Program Files\Google\Chrome\Application/chrome.exe')   #location of your file
         elif("open command prompt" in data or 'open cmd' in data or "open command" in data):
             print("opening Command Prompt")
             os.startfile("C:/Users/91785/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/System Tools/Command Prompt.lnk")
-            continue
         elif ("open code" in data or "open good" in data or "open cold" in data or "open vs code" in data or "open vidual studio code" in data or "open visual studio" in data or "open court" in data):
             print("opening Visual Studio Code")
             os.startfile("C:/Users/91785/AppData/Local/Programs/Microsoft VS Code/Code.exe")    #location of your file
-            continue
         elif ("open pycharm" in data or "start pycharm" in data):
             os.system('pycharm')
             ''' this methode returns same as what CMD returns.
@@ -73,9 +68,9 @@ while True:
         elif ("open android studio" in data):
             os.startfile("C:\Program Files\Android\Android Studio/bin\studio64")
             print("Opening Android Studio")
-        elif ("open control panel" in data or "control panel open" in data):
+        elif ("open control panel" in data):
             os.startfile("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Immersive Control Panel.lnk")
-        elif ("open youtube" in data or "youtube open" in data):
+        elif ("open youtube" in data):
             webbrowser.open("https://youtube.com/")
         elif ("take screenshot" in data or "take a screenshot" in data or "screenshot take" in data or "take screenshots" in data):
             try: takescreenshot1()
